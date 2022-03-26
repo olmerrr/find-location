914225f1bb4491c24692d9b8325e0a9c68d88e9e
yaroslav <dombrovskiy91@gmail.com>
2022-03-26 15:07:03 +0200

    fix bag with keypress Enter + add data in html

--- src/index.js
+++ src/index.js
@@ -2,21 +2,40 @@ import { validateIP } from "./helpers";
 
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
+const ipInfo = document.getElementById('ip');
+const locationInfo = document.getElementById('location');
+const utcInfo = document.getElementById('utc');
+const ispInfo = document.getElementById('isp');
 
+// console.log(ipInfo,locationInfo,utcInfo,ispInfo);
 ipInput.addEventListener("keydown", handleKey);
 btn.addEventListener("click", getData);
 
-function getData() {
-  if (validateIP(ipInput.value)) {
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_KyPVjpK6mleYPpUP0EOwsoaOItHM8&ipAddress=${ipInput.value}`)
-    .then(response => response.json()
-    .then(console.log))
-  }
- }
- 
 function handleKey(event) {
-  if (event === 'Enter'){
+  if (event.key === 'Enter'){
     getData();
   }
+}
+
+ function getData() {
+ 
+  if (validateIP(ipInput.value)) {
+    const response =  fetch(`
+    https://geo.ipify.org/api/v2/country?apiKey=at_KyPVjpK6mleYPpUP0EOwsoaOItHM8&ipAddress=${ipInput.value}
+    `)
+    .then(response => response.json())
+    .then(setInfo)
+  }
+
+ }
+ 
+
+
+function setInfo(mapData){
+  console.log(mapData)
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = `${mapData.location.country} ${mapData.location.region}`;  
+  utcInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
  
